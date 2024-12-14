9ed0e4c26f085dd745720f6aea85ef8689ff4dab
MrinalSisodia <mrinalsisodia28@gmail.com>
2024-12-14 20:45:26 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+let a = document.createElement("p")
+a.innerText = "Hello World"
+document.getElementById("app").appendChild(a)
+
+// API call to servers of OMDB 
+const movieData = fetch("http://www.omdbapi.com/?i=tt12735488&apikey=8c3131ae")
+
+// Try
+// movieData.then(response => {
+//     response.json().then(data => {  
+//     })
+// })
+
+
+async function app () {
+    let response = await movieData;
+    let data = await response.json()
+    let AppEle = document.getElementById("app")
+    // Data - 
+    console.log(data)
+    
+    
+    let title = data.Title;
+
+    let actors = data.Actors;
+
+    let poster = data.Poster;
+
+    let TileEle = document.createElement("h3")
+    TileEle.innerText = data.Title;
+
+    let ActorsEle = document.createElement("p");
+    ActorsEle.innerText = data.Actors;
+
+    let PosterEle = document.createElement("img")
+    PosterEle.setAttribute("src", poster);
+
+    AppEle.appendChild(PosterEle)
+    AppEle.appendChild(ActorsEle)
+    AppEle.appendChild(TileEle)
+    
+
+    console.log(document.getElementById("app"))
+}
+
+app();
